a2af9ebb3db62a5a05056aae56b5d12ef9c7c34c
rodjunger <rod.jun.ger@gmail.com>
2023-07-05 14:43:38 -0300

    Solução para o quinto desafio adicionada

--- challanges/anagram_of_palindrome.go
+++ challanges/anagram_of_palindrome.go
@@ -1,1 +1,24 @@
 package pwc
+
+// IsAnagramOfPalindrome retorna "true" se a string in pode ser rescrita como um palindromo, e "false" caso contrário
+// A ideia é simplesmente contar o numero de caracteres com quantidade impar na string, se tivermos mais de um,
+// Então é impossivel criar um palindromo, a contagem de pares não importa porque sempre podemos encaixar
+// quantidades iguais na "direita" e na "esquerda" do palindromo para casar os caracteres
+func IsAnagramOfPalindrome(in string) string {
+	countOfRunes := map[rune]int{}
+	for _, character := range in {
+		countOfRunes[character] += 1
+	}
+
+	cnt := 0
+
+	for _, v := range countOfRunes {
+		if v%2 != 0 {
+			cnt += 1
+			if cnt > 1 {
+				return "false"
+			}
+		}
+	}
+	return "true"
+}
---
+++ challanges/anagram_of_palindrome_test.go
@@ -0,0 +1,31 @@
+package pwc
+
+import "testing"
+
+func TestIsAnagramOfPalindrome(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"racecar", "true"},
+		{"rraacec", "true"},
+		{"", "true"},
+		{"a", "true"},
+		{"abc", "false"},
+		{"aab", "true"},
+		{"aabb", "true"},
+		{"aabcc", "true"},
+		{"aabccd", "false"},
+		{"aaabbcc", "true"},
+		{"aaabbccd", "false"},
+		{"tactcoa", "true"},
+		{"tactcoaa", "false"},
+	}
+
+	for _, test := range tests {
+		result := IsAnagramOfPalindrome(test.input)
+		if result != test.expected {
+			t.Errorf("IsAnagramOfPalindrome(%q) returned %q, expected %q", test.input, result, test.expected)
+		}
+	}
+}
